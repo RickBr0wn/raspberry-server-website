9ea33ec0563cebc0f6350fdb59363dfa2416ec01
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-07 18:29:06 +0000

    Added error handling for mongoDB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var login = require('./routes/login');
@@ -13,6 +14,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// mongoDB connection
+mongoose.connect('mongodb://localhost:27017/homedb');
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'DATABASE CONNECTION ERROR: '));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
