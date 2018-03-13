9ebcbbfa2fcf95e99935c535f8ea9f31909fc208
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-13 15:29:08 +0000

    Installed dependencies (johnny-5 & socket.io)

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// Global Variables
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var io = require('socket.io');
+var five = require("johnny-five");
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
